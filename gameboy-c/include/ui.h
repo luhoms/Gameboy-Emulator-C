ff723ee1d718ca8ad7fb6d43f3f9e3bcb0697a8c
Liam Hooks <luhom9678@gmail.com>
2025-05-05 20:19:32 -0400

    initial commit

---
+++ gameboy-c/include/ui.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <common.h>
+
+static const int SCREEN_WIDTH = 700;
+static const int SCREEN_HEIGHT = 700;
+
+void ui_init();
+void ui_handle_events();
+void ui_update();
---
+++ gameboy-c/lib/emu.c
@@ -0,0 +1,130 @@
+#include <stdio.h>
+#include <emu.h>
+#include <cart.h>
+#include <cpu.h>
+#include <ui.h>
+#include <timer.h>
+#include <dma.h>
+#include <ppu.h>
+
+
+#ifdef _WIN32
+#   include <windows.h>     /* Sleep, HANDLE, etc. */
+    typedef HANDLE pthread_t;
+
+    static int pthread_create(pthread_t *th, void *attr,
+                              void *(*fn)(void *), void *arg)
+    {
+        (void)attr;    /* unused */
+        *th = CreateThread(NULL, 0,
+                           (LPTHREAD_START_ROUTINE)fn, arg,
+                           0, NULL);
+        return *th == NULL;            /* 0 on success, like POSIX */
+    }
+
+    static int pthread_join(pthread_t th, void **ret)
+    {
+        WaitForSingleObject(th, INFINITE);
+        CloseHandle(th);
+        if (ret) *ret = NULL;
+        return 0;
+    }
+
+    /* usleep is microseconds; Sleep is milliseconds */
+#   define usleep(usec) Sleep((DWORD)((usec) / 1000))
+#else
+#   include <pthread.h>
+#   include <unistd.h>
+#endif
+
+/* 
+  Emu components:
+
+  |Cart|
+  |CPU|
+  |Address Bus|
+  |PPU|
+  |Timer|
+
+*/
+
+static emu_context ctx;
+
+emu_context *emu_get_context() {
+    return &ctx;
+}
+
+void *cpu_run(void *p) {
+    timer_init();
+    cpu_init();
+    ppu_init();
+
+    ctx.running = true;
+    ctx.paused = false;
+    ctx.ticks = 0;
+
+    while(ctx.running) {
+        if (ctx.paused) {
+            delay(10);
+            continue;
+        }
+
+        if (!cpu_step()) {
+            printf("CPU Stopped\n");
+            return 0;
+        }
+    }
+
+    return 0;
+}
+
+int emu_run(int argc, char **argv) {
+    if (argc < 2) {
+        printf("Usage: emu <rom_file>\n");
+        return -1;
+    }
+
+    if (!cart_load(argv[1])) {
+        printf("Failed to load ROM file: %s\n", argv[1]);
+        return -2;
+    }
+
+    printf("Cart loaded..\n");
+
+    ui_init();
+    
+    pthread_t t1;
+
+    if (pthread_create(&t1, NULL, cpu_run, NULL)) {
+        fprintf(stderr, "FAILED TO START MAIN CPU THREAD!\n");
+        return -1;
+    }
+
+    u32 prev_frame = 0;
+
+    while(!ctx.die) {
+        usleep(1000);
+        ui_handle_events();
+
+        if (prev_frame != ppu_get_context()->current_frame) {
+            ui_update();
+        }
+
+        prev_frame = ppu_get_context()->current_frame;
+    }
+
+    return 0;
+}
+
+void emu_cycles(int cpu_cycles) {
+    
+    for (int i=0; i<cpu_cycles; i++) {
+        for (int n=0; n<4; n++) {
+            ctx.ticks++;
+            timer_tick();
+            ppu_tick();
+        }
+
+        dma_tick();
+    }
+}
